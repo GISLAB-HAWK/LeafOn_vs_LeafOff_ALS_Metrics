a6d6b5c728d72616c68c2b2babc7b6ab86689588
FloFranz <florian.franz86@gmx.de>
2026-07-13 13:46:31 +0200

    add canopy gap detection script

---
+++ scripts/canopy_gaps.R
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------------
+# Name:         canopy_gaps.R
+# Description:  Detection of canopy gaps in airborne laser scanning (ALS)-based
+#               canopy height models (CHM) in leaf-on and leaf-off condition.
+#               The detected canopy gaps are cropped to forest inventory plots,
+#               and the gap fraction per plot is calculated.
+# Author:       Florian Franz
+# Contact:      florian.franz@nw-fva.de
+#-------------------------------------------------------------------------------
